84b738dd1477672efc36ff933ba561f65b284fdf
shameyang <shameyang@outlook.com>
2024-05-06 20:40:37 +0800

    前端路由整合

--- friendhub-frontend/src/layouts/BasicLayout.vue
+++ friendhub-frontend/src/layouts/BasicLayout.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import Index from "../pages/Index.vue";
-import Team from "../pages/Team.vue";
-import User from "../pages/User.vue";
 
 const onClickLeft = () => history.back();
 
-const active = ref("index");
-const onChange = (index) => showToast(`${index}`);
 </script>
 
 <template>
@@ -17,14 +11,12 @@ const onChange = (index) => showToast(`${index}`);
     </template>
   </van-nav-bar>
   <div id="content">
-    <template v-if="active === 'index'"><Index /></template>
-    <template v-if="active === 'team'"><Team /></template>
-    <template v-if="active === 'user'"><User /></template>
+    <router-view />
   </div>
-  <van-tabbar v-model="active" @change="onChange">
-    <van-tabbar-item icon="home-o" name="index"></van-tabbar-item>
-    <van-tabbar-item icon="search" name="team"></van-tabbar-item>
-    <van-tabbar-item icon="contact-o" name="user"></van-tabbar-item>
+  <van-tabbar route>
+    <van-tabbar-item to="/" icon="home-o"></van-tabbar-item>
+    <van-tabbar-item to="/team" icon="search"></van-tabbar-item>
+    <van-tabbar-item to="/user" icon="contact-o"></van-tabbar-item>
   </van-tabbar>
 </template>
 
