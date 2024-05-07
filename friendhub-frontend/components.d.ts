dc615c8b0ff57346d51f2f274a1717aea10fbe37
shameyang <shameyang@outlook.com>
2024-05-07 20:07:32 +0800

    前端搜索页面开发

--- friendhub-frontend/components.d.ts
+++ friendhub-frontend/components.d.ts
@@ -9,9 +9,16 @@ declare module 'vue' {
   export interface GlobalComponents {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    VanButton: typeof import('vant/es')['Button']
+    VanCol: typeof import('vant/es')['Col']
+    VanDivider: typeof import('vant/es')['Divider']
     VanIcon: typeof import('vant/es')['Icon']
     VanNavBar: typeof import('vant/es')['NavBar']
+    VanRow: typeof import('vant/es')['Row']
+    VanSearch: typeof import('vant/es')['Search']
     VanTabbar: typeof import('vant/es')['Tabbar']
     VanTabbarItem: typeof import('vant/es')['TabbarItem']
+    VanTag: typeof import('vant/es')['Tag']
+    VanTreeSelect: typeof import('vant/es')['TreeSelect']
   }
 }
--- friendhub-frontend/src/layouts/BasicLayout.vue
+++ friendhub-frontend/src/layouts/BasicLayout.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
+import {useRouter} from "vue-router";
+
+const router = useRouter();
 
 const onClickLeft = () => history.back();
-
+const onClickRight = () => {
+  router.push('/search');
+};
 </script>
 
 <template>
-  <van-nav-bar title="标题" left-text="" left-arrow @click-left="onClickLeft">
+  <van-nav-bar
+      title="FriendHub"
+      left-text=""
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+  >
     <template #right>
       <van-icon name="search" size="18"/>
     </template>
   </van-nav-bar>
   <div id="content">
-    <router-view />
+    <router-view/>
   </div>
   <van-tabbar route>
     <van-tabbar-item to="/" icon="home-o"></van-tabbar-item>
