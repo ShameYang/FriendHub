a350becf47be8fcae20792f1bf881c47cb28421f
shameyang <shameyang@outlook.com>
2024-05-09 16:34:54 +0800

    前端用户列表页开发

--- friendhub-frontend/src/config/route.ts
+++ friendhub-frontend/src/config/route.ts
@@ -3,12 +3,14 @@ import Team from "../pages/Team.vue";
 import User from "../pages/User.vue";
 import Search from "../pages/Search.vue"
 import UserEdit from "../pages/UserEdit.vue";
+import SearchResult from "../pages/SearchResult.vue";
 
 const routes = [
     { path: '/', component: Index },
     { path: '/team', component: Team },
     { path: '/user', component: User },
     { path: '/user/edit', component: UserEdit },
+    { path: '/user/list', component: SearchResult },
     { path: '/search', component: Search },
 ]
 
---
+++ friendhub-frontend/src/pages/SearchResult.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import {ref} from "vue";
+
+const route = useRoute();
+
+const { tags } = route.query;
+
+const mockUser = {
+  id: 1,
+  userName: 'shameyang',
+  userAccount: '123321',
+  avatarUrl: 'https://avatars.githubusercontent.com/u/94451620?v=4',
+  gender: '男',
+  phone: '123123',
+  email: '111@qq.com',
+  createTime: new Date(),
+  tags: ['java', '学习中', '大三', '待实习'],
+  profile: '一个热爱编程的大学生，持续学习中...'
+}
+
+const userList = ref([mockUser]);
+</script>
+
+<template>
+  <van-card
+      v-for="user in userList"
+      :title="user.userName"
+      :desc="user.profile"
+      :thumb="user.avatarUrl"
+  >
+    <template #tags>
+      <van-tag plain type="primary" v-for="tag in user.tags" style="margin-right: 8px; margin-top: 8px">
+        {{tag}}
+      </van-tag>
+    </template>
+    <template #footer>
+      <van-button size="small">联系我</van-button>
+    </template>
+  </van-card>
+</template>
+
+<style scoped>
+
+</style>
