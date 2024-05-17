f74a247088adddfce8b66a4b65e598aaa1c3f411
shameyang <shameyang@outlook.com>
2024-05-17 16:46:57 +0800

    add 前端主页开发

--- friendhub-frontend/src/pages/Index.vue
+++ friendhub-frontend/src/pages/Index.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+import myAxios from '../plugins/myAxios'
+import { showFailToast } from "vant";
+import { UserType } from "../models/user";
+import UserCardList from "../components/UserCardList.vue";
 
+const userList = ref([]);
+
+onMounted(async () => {
+  const userListData = await myAxios.get('/user/recommend', {
+    params: {},
+  })
+      .then(function (response) {
+        console.log('/user/recommend   succeed', response);
+        showSuccessToast('请求成功');
+        return response?.data;
+      })
+      .catch(function (error) {
+        console.error('/user/recommend', error);
+        showFailToast('请求失败');
+      })
+  if (userListData) {
+    userListData.forEach((user: UserType) => {
+      if (user.tags) {
+        user.tags = JSON.parse(user.tags);
+      }
+    })
+    userList.value = userListData;
+  }
+})
 </script>
 
 <template>
-  主页
+  <user-card-list :user-list="userList"/>
+  <van-empty v-if="!userList || userList.length < 1" description="数据为空" />
 </template>
 
 <style scoped>
