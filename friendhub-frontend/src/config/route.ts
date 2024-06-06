8612cf8030cfdc2f863f265836e547874f81d0bf
shameyang <shameyang@outlook.com>
2024-06-06 18:13:17 +0800

    add 前端创建队伍页面开发

--- friendhub-frontend/src/config/route.ts
+++ friendhub-frontend/src/config/route.ts
@@ -5,10 +5,12 @@ import Search from "../pages/Search.vue"
 import UserEdit from "../pages/UserEdit.vue";
 import SearchResult from "../pages/SearchResult.vue";
 import UserLogin from "../pages/UserLogin.vue";
+import TeamAdd from "../pages/TeamAdd.vue";
 
 const routes = [
     { path: '/', component: Index },
     { path: '/team', component: Team },
+    { path: '/team/add', component: TeamAdd },
     { path: '/user', component: User },
     { path: '/user/edit', component: UserEdit },
     { path: '/user/list', component: SearchResult },
