3e900ae8ccb01ed17ffe21c007cb519043e75318
shameyang <shameyang@outlook.com>
2024-06-09 22:12:51 +0800

    add 前端添加搜索队伍功能

--- friendhub-frontend/src/pages/Team.vue
+++ friendhub-frontend/src/pages/Team.vue
@@ -7,6 +7,12 @@ import { showFailToast } from "vant";
 import TeamCardList from "../components/TeamCardList.vue";
 
 const router = useRouter();
+const searchText = ref('');
+
+// 搜索队伍
+const onSearch = (val) => {
+  listTeam(val);
+};
 
 // 跳转到创建队伍页
 const toAddTeam = () => {
@@ -17,20 +23,32 @@ const toAddTeam = () => {
 
 const teamList = ref([]);
 
-// 页面加载时只触发一次
-onMounted(async () => {
-  const res = await myAxios.get("/team/list");
+// 加载队伍信息
+const listTeam = async (val = '', status = 0) => {
+  const res = await myAxios.get("/team/list", {
+    params: {
+      searchText: val,
+      pageNum: 1,
+      status,
+    },
+  });
   if (res?.code === 0) {
     teamList.value = res.data;
   } else {
     showFailToast('加载队伍失败，请刷新重试');
   }
+}
+
+// 页面加载时只触发一次
+onMounted(async () => {
+  listTeam();
 })
 
 </script>
 
 <template>
   <div id="teamPage">
+    <van-search v-model="searchText" placeholder="搜索队伍" @search="onSearch"/>
     <van-button type="primary" @click="toAddTeam">创建队伍</van-button>
     <team-card-list :teamList="teamList"/>
     <van-empty v-if="teamList?.length < 1" description="数据为空"/>
