5eb43ab0fa2ff024c7931e6b82a2eee1599edb8d
shameyang <shameyang@outlook.com>
2024-06-19 15:31:03 +0800

    add 前端主页：添加匹配模式、加载骨架屏特效

--- friendhub-frontend/src/components/UserCardList.vue
+++ friendhub-frontend/src/components/UserCardList.vue
@@ -1,32 +1,35 @@
 <script setup lang="ts">
-  import { UserType } from "../models/user";
+import { UserType } from "../models/user";
 
-  interface UserCardListProps {
-    userList: UserType[];
-  }
+interface UserCardListProps {
+  loading: boolean;
+  userList: UserType[];
+}
 
-  const props = withDefaults(defineProps<UserCardListProps>(), {
-    // @ts-ignore
-    userList: [] as UserType[],
-  });
+const props = withDefaults(defineProps<UserCardListProps>(), {
+  loading: true,
+  // @ts-ignore
+  userList: [] as UserType[],
+});
 </script>
 
 <template>
-  <van-card
-      v-for="user in props.userList"
-      :title="user.userName"
-      :desc="user.profile"
-      :thumb="user.avatarUrl"
-  >
-    <template #tags>
-      <van-tag plain type="primary" v-for="tag in user.tags" style="margin-right: 8px; margin-top: 8px">
-        {{tag}}
-      </van-tag>
-    </template>
-    <template #footer>
-      <van-button size="small">联系我</van-button>
-    </template>
-  </van-card>
+  <van-skeleton title avatar :row="3" :loading="props.loading" v-for="user in props.userList">
+    <van-card
+        :title="user.userName"
+        :desc="user.profile"
+        :thumb="user.avatarUrl"
+    >
+      <template #tags>
+        <van-tag plain type="primary" v-for="tag in user.tags" style="margin-right: 8px; margin-top: 8px">
+          {{ tag }}
+        </van-tag>
+      </template>
+      <template #footer>
+        <van-button size="small">联系我</van-button>
+      </template>
+    </van-card>
+  </van-skeleton>
 </template>
 
 <style scoped>
--- friendhub-frontend/src/pages/Index.vue
+++ friendhub-frontend/src/pages/Index.vue
@@ -1,28 +1,56 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { ref, watchEffect } from "vue";
 import myAxios from '../plugins/myAxios'
 import { showFailToast } from "vant";
 import { UserType } from "../models/user";
 import UserCardList from "../components/UserCardList.vue";
 
-const userList = ref([]);
+const isMatchMode = ref<boolean>(false);
 
-onMounted(async () => {
-  const userListData = await myAxios.get('/user/recommend', {
-    params: {
-      pageSize: 8,
-      pageNum: 1,
-    },
-  })
-      .then(function (response) {
-        console.log('/user/recommend   succeed', response);
-        showSuccessToast('请求成功');
-        return response?.data?.records;
-      })
-      .catch(function (error) {
-        console.error('/user/recommend', error);
-        showFailToast('请求失败');
-      })
+const userList = ref([]);
+const loading = ref(true);
+
+/**
+ * 加载数据
+ */
+const loadData = async () => {
+  let userListData;
+  loading.value = true;
+  // 匹配模式
+  if (isMatchMode.value) {
+    const num = 10;
+    userListData = await myAxios.get('/user/match', {
+      params: {
+        num,
+      },
+    })
+        .then(function (response) {
+          console.log('/user/match succeed', response);
+          return response?.data;
+        })
+        .catch(function (error) {
+          console.error('/user/match error', error);
+          showFailToast('请求失败');
+        })
+
+  } else {
+    // 普通模式
+    userListData = await myAxios.get('/user/recommend', {
+      params: {
+        pageSize: 8,
+        pageNum: 1,
+      },
+    })
+        .then(function (response) {
+          console.log('/user/recommend   succeed', response);
+          showSuccessToast('请求成功');
+          return response?.data?.records;
+        })
+        .catch(function (error) {
+          console.error('/user/recommend', error);
+          showFailToast('请求失败');
+        })
+  }
   if (userListData) {
     userListData.forEach((user: UserType) => {
       if (user.tags) {
@@ -31,11 +59,21 @@ onMounted(async () => {
     })
     userList.value = userListData;
   }
+  loading.value = false;
+}
+
+watchEffect(() => {
+  loadData();
 })
 </script>
 
 <template>
-  <user-card-list :user-list="userList"/>
+  <van-cell center title="匹配模式">
+    <template #right-icon>
+      <van-switch v-model="isMatchMode" size="24"/>
+    </template>
+  </van-cell>
+  <user-card-list :user-list="userList" :loading="loading"/>
   <van-empty v-if="!userList || userList.length < 1" description="数据为空" />
 </template>
 
